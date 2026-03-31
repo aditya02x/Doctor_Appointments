c937abf6c1b8cdf697ee1458c24143b65be88c09
Aditya Raj <adityaraj34@icloud.com>
2026-03-31 23:55:03 +0530

    server start

--- Server.js
+++ Server.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import { Server } from 'socket.io';
-import http from 'http';    
-import { start } from 'repl';
 
+const app = express();
 
-start
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });   

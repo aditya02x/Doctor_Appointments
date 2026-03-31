716951c630759fc8e0ecf124cc49ff43cc6ca030
Aditya Raj <adityaraj34@icloud.com>
2026-03-31 23:56:01 +0530

    server

--- Server.js
+++ Server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { connectDB } from './DB/db';
 
 const app = express();
 
@@ -9,6 +10,13 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 const startServer = () => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });   
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Error starting the server:", error);
+    }
+}
+ 

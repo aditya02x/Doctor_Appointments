e8023fef1af5b292b4036d9519fcb7f825f7cd11
Aditya Raj <adityaraj34@icloud.com>
2026-04-01 11:42:55 +0530

    appoiment schema done

---
+++ models/appointment.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const appointmentSchema = new mongoose.Schema({
+    patientId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    doctorId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Doctor",
+        required:true
+    },
+    slotId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Slot',
+        required:true
+    },
+    status:{
+        type:String,
+        enum:['pendind','completed','cancelled'],
+        required:true
+    },
+   paymentStatus:   {
+     type: String,
+     enum: ['unpaid', 'paid'],
+      default: 'unpaid'
+     },
+     paymentId:
+      {
+         type: String,
+          default: ''
+         },
+          amount:
+          { type: Number,
+             required: true
+             },
+             appointmentDate:
+             { type: Date, 
+                required: true 
+            },
+
+})

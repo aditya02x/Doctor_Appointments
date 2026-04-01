adab408217174322d0a02288f7f269637e80e712
Aditya Raj <adityaraj34@icloud.com>
2026-04-01 11:18:18 +0530

    Doctor schema done

---
+++ models/Doctor.model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose '
+
+const doctorSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    specialization:{
+        type:String,
+        required:true
+    },
+    experience:{
+        type:Number,
+        required:true
+    },
+    fees:{
+        type:Number,
+        require:true
+    },
+    profilePic:{
+        type:String,
+        default:''
+    },
+    isApproved:{
+        type:Boolean,
+        default:false
+    }
+})
+
+export default mongoose.model('Doctor',doctorSchema)

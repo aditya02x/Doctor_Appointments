31e63625463d2a23adc5f8d52bfe49e99c71564e
Aditya Raj <adityaraj34@icloud.com>
2026-04-03 12:24:45 +0530

    payement route done

---
+++ routes/payment.route.js
@@ -0,0 +1,8 @@
+import express from 'express '
+import { createOrder, getKey, verifyPayment } from '../controllers/payements.controller'
+const router = express.Router()
+
+
+router.get('/key',getKey)
+router.post('create-order',createOrder)
+router.post('/verify',verifyPayment)
